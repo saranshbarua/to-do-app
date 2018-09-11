085b1b305581ed17ebcb167d02a58c49af047cc0
saranshbarua <saranshbarua@gmail.com>
2018-09-12 01:05:46 +0530

    retain state with browser's local storage

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -16,6 +16,46 @@ class ToDoList extends React.Component {
 		this.deleteItem = this.deleteItem.bind(this);
 	}
 
+
+	componentDidMount() {
+	    this.hydrateStateWithLocalStorage();
+
+	    window.addEventListener(
+	      "beforeunload",
+	      this.saveStateToLocalStorage.bind(this)
+	    );
+	  }
+
+	  componentWillUnmount() {
+	    window.removeEventListener(
+	      "beforeunload",
+	      this.saveStateToLocalStorage.bind(this)
+	    );
+
+	    this.saveStateToLocalStorage();
+	  }
+
+	  hydrateStateWithLocalStorage() {
+	    for (let key in this.state) {
+	      if (localStorage.hasOwnProperty(key)) {
+	        let value = localStorage.getItem(key);
+
+	        try {
+	          value = JSON.parse(value);
+	          this.setState({ [key]: value });
+	        } catch (e) {
+	          // handle empty string
+	          this.setState({ [key]: value });
+	        }
+	      }
+	    }
+	  }
+
+	  saveStateToLocalStorage() {
+	    for (let key in this.state) {
+	      localStorage.setItem(key, JSON.stringify(this.state[key]));
+	    }
+	  }
 	
 
 	addItem(e) {
